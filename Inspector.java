7d5f1cca5b2d6cf59de477f9304cdc99bff3b934
unknown <rsbarhey@ZONE05-WC.cs.ucalgary.ca>
2015-10-16 14:18:16 -0600

    Added the class Inspector with sekeltons I thought was required

---
+++ Inspector.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.*;
+
+public class Inspector 
+{
+	public void inspect(Object obj, boolean recursive)
+	{
+		
+	}
+	
+	private void getConstructors(Object obj)
+	{
+		
+	}
+	
+	private void getFileds(Object obj)
+	{
+
+	}
+	
+	private void getMethods(Object obj)
+	{
+		
+	}
+	
+	private void getInterfaces(Object obj)
+	{
+		
+	}
+	
+	private void getSuperClass(Object obj)
+	{
+		
+	}
+}
