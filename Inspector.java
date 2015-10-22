a274585b62ff5683ea2bf5ec0d8a2a79f928f015
unknown <rsbarhey@ZONE20-WB.cs.ucalgary.ca>
2015-10-22 14:01:57 -0600

    Refactoring rename methods

--- Inspector.java
+++ Inspector.java
@@ -28,26 +28,26 @@ public class Inspector
 		getMethods(obj);
 		getConstructors(obj);
 
-		printInspectedName(inspectedClass, "Inspecting class: \t\t");
-		printInspectedName(inspectedSuperClass, "Imidiate super class: \t\t");
+		printObjectWithHeader(inspectedClass, "Inspecting class: \t\t");
+		printObjectWithHeader(inspectedSuperClass, "Imidiate super class: \t\t");
 		System.out.println("-----------------------------------------------------------------------------------------------");
-		printListOfInspectedNames(inspectedInterfaces, "Imidiate interfaces: \t\t");
+		printListOfObjects(inspectedInterfaces, "Imidiate interfaces: \t\t");
 		System.out.println("-----------------------------------------------------------------------------------------------");
-		printListOfInspectedNames(inspectedConstructors, "Declared constructors: \t\t");
+		printListOfObjects(inspectedConstructors, "Declared constructors: \t\t");
 		System.out.println("-----------------------------------------------------------------------------------------------");
-		printListOfInspectedNames(inspectedMethods, "Declared methods: \t\t");
+		printListOfObjects(inspectedMethods, "Declared methods: \t\t");
 		System.out.println("-----------------------------------------------------------------------------------------------");
-		printListOfInspectedNames(inspectedFields, "Declared fields: \t\t");
+		printListOfObjects(inspectedFields, "Declared fields: \t\t");
 		
 		//Traversing the hierarchy
 		System.out.println("_______________________________________________________________________________________________");
-		printInspectedName(inspectedSuperClass, "Inspecting imidiate super: \t\t");
+		printObjectWithHeader(inspectedSuperClass, "Inspecting imidiate super: \t\t");
 		traverseSuperClass(inspectedSuperClass, inspectedClass);
 		System.out.println("_______________________________________________________________________________________________");
 		System.out.println();
 		
 		System.out.println("_______________________________________________________________________________________________");
-		printListOfInspectedNames(inspectedInterfaces, "Inspecteing imidiate interfaces: \t\t");
+		printListOfObjects(inspectedInterfaces, "Inspecteing imidiate interfaces: \t\t");
 		traverseInterfaces(inspectedInterfaces, inspectedClass);
 		System.out.println("_______________________________________________________________________________________________");
 		System.out.println();
@@ -57,12 +57,12 @@ public class Inspector
 		System.out.println("===============================================================================================");
 	}
 
-	private void printInspectedName(Object inpsectedObject, String header)
+	private void printObjectWithHeader(Object inpsectedObject, String header)
 	{
 		System.out.println(header + inpsectedObject);
 	}
 
-	private void printListOfInspectedNames(Object[] inspectedObjects, String header) 
+	private void printListOfObjects(Object[] inspectedObjects, String header) 
 	{
 		if (inspectedObjects.length == 0)
 		{
@@ -72,7 +72,7 @@ public class Inspector
 		{
 			System.out.printf(header + "%d \n", inspectedObjects.length);
 			for (int i = 0; i < inspectedObjects.length; i++) {
-				printInspectedName(inspectedObjects[i], "");
+				printObjectWithHeader(inspectedObjects[i], "");
 			}
 		}
 	}
@@ -85,9 +85,9 @@ public class Inspector
 		}
 		System.out.println("-----------------------------------------------------------------------------------------------");
 		System.out.println(extendingClass.getName() + " extends class " + superClass.getName());
-		printListOfInspectedNames(superClass.getDeclaredMethods(), "Methods: \t\t");
+		printListOfObjects(superClass.getDeclaredMethods(), "Methods: \t\t");
 		System.out.println("");
-		printListOfInspectedNames(superClass.getDeclaredFields(), "Fields: \t\t");
+		printListOfObjects(superClass.getDeclaredFields(), "Fields: \t\t");
 		System.out.println("-----------------------------------------------------------------------------------------------");
 		traverseInterfaces(superClass.getInterfaces(), superClass);
 		traverseSuperClass(superClass.getSuperclass(), superClass);
@@ -103,9 +103,9 @@ public class Inspector
 		{
 			System.out.println("-----------------------------------------------------------------------------------------------");
 			System.out.println(extendingClass.getName() +" extends interface " + interfaces[i].getName());
-			printListOfInspectedNames(interfaces[i].getDeclaredMethods(), "Implemented methods: \t\t");
+			printListOfObjects(interfaces[i].getDeclaredMethods(), "Implemented methods: \t\t");
 			System.out.println("");
-			printListOfInspectedNames(interfaces[i].getDeclaredFields(), "Constant fields: \t\t");
+			printListOfObjects(interfaces[i].getDeclaredFields(), "Constant fields: \t\t");
 			System.out.println("-----------------------------------------------------------------------------------------------");
 			traverseInterfaces(interfaces[i].getInterfaces(), interfaces[i]);
 		}
@@ -133,7 +133,7 @@ public class Inspector
 				
 					if(value == null)
 					{
-						printInspectedName(value, fields[i].getName() + " = ");
+						printObjectWithHeader(value, fields[i].toString() + " = ");
 					}
 				
 					else if(value.getClass().isArray())
@@ -143,19 +143,19 @@ public class Inspector
 				
 					else if (Arrays.asList(PRIMITIVE_WRAPPERS).contains(value.getClass()) || value.getClass().isPrimitive())
 					{
-						printInspectedName(value, fields[i].getName() + " = ");
+						printObjectWithHeader(value, fields[i].toString() + " = ");
 					}
 				
 					else if(recursive)
 					{
 						System.out.println();
-						printFieldsValuesRecursively(fields[i].getName(), fields[i].getType(), value);
+						printFieldsValuesRecursively(fields[i].toString(), fields[i].getType(), value);
 						System.out.println();
 					}
 				
 					else
 					{
-						printInspectedName(value.hashCode(), fields[i].getName() + " address(HashCode) is ");
+						printObjectWithHeader(value.hashCode(), fields[i].toString() + " address(HashCode) is ");
 					}
 				} 
 				catch (IllegalArgumentException e) {
@@ -185,19 +185,21 @@ public class Inspector
 		}
 		
 		//before going deeper we need to check if the member has a super class that has fields needed to be printed
-		// we also check if it's not of type Comparator
+		//we also check if it's not of type Comparator
 		if(type != null && type != Comparator.class && !(Arrays.asList(PRIMITIVE_WRAPPERS).contains(fieldValue.getClass()) || fieldValue.getClass().isPrimitive()))
 		{
 			if(!type.getSuperclass().equals(Object.class))
 			{
 				type = type.getSuperclass();
+				System.out.println(memberName + " has super class: " + type.getName() + " Inspecting deeper ..." );
 				printFieldsValues(type.getDeclaredFields(), fieldValue, type, true);
+				System.out.println();
 			}
 		}
 		
 		if(fieldValue == null || Arrays.asList(PRIMITIVE_WRAPPERS).contains(fieldValue.getClass()) || fieldValue.getClass().isPrimitive())
 		{
-			printInspectedName(fieldValue, memberName + " = ");
+			printObjectWithHeader(fieldValue, memberName + " = ");
 		}
 		else if(fieldValue.getClass().isArray())
 		{
@@ -208,7 +210,7 @@ public class Inspector
 			{
 				if(Arrays.asList(PRIMITIVE_WRAPPERS).contains(fieldValue.getClass().getComponentType()) || fieldValue.getClass().getComponentType().isPrimitive() || Array.get(fieldValue, index) == null)
 				{
-					printInspectedName(Array.get(fieldValue, index), memberName + "[" + Integer.toString(index)+ "] = ");
+					printObjectWithHeader(Array.get(fieldValue, index), memberName + "[" + Integer.toString(index)+ "] = ");
 				}
 				
 				else
@@ -233,7 +235,7 @@ public class Inspector
 					}
 					Object value = fieldMembers[i].get(fieldValue);
 					printedObjectsValues.add(fieldValue);
-					printFieldsValuesRecursively(fieldMembers[i].getName(), fieldMembers[i].getType(), value);
+					printFieldsValuesRecursively(fieldMembers[i].toString(), fieldMembers[i].getType(), value);
 				} catch (IllegalArgumentException e) {
 					e.printStackTrace();
 				} catch (IllegalAccessException e) {
@@ -252,7 +254,7 @@ public class Inspector
 		{
 			if(Arrays.asList(PRIMITIVE_WRAPPERS).contains(obj.getClass().getComponentType()) || obj.getClass().getComponentType().isPrimitive() || Array.get(obj, index) == null)
 			{
-				printInspectedName(Array.get(obj, index), obj.getClass().getName() + "[" + Integer.toString(index)+ "] = ");
+				printObjectWithHeader(Array.get(obj, index), obj.getClass().getName() + "[" + Integer.toString(index)+ "] = ");
 			}
 			
 			else if(recursive)
@@ -262,7 +264,7 @@ public class Inspector
 			
 			else
 			{
-				printInspectedName(Array.get(obj, index).hashCode(), obj.getClass().getName() + "[" + Integer.toString(index)+ "] address (HashCode) ");	
+				printObjectWithHeader(Array.get(obj, index).hashCode(), obj.getClass().getName() + "[" + Integer.toString(index)+ "] address (HashCode) ");	
 			}
 		}
 		System.out.println();
